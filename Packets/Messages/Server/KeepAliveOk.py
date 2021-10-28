fa031bb44d25615e0afb8d17640d820a196efcf0
Romashka1337 <80623570+RomashkaTea@users.noreply.github.com>
2021-10-29 01:49:15 +0300

    keep alive ok fix

--- Packets/Messages/Server/KeepAliveOk.py
+++ Packets/Messages/Server/KeepAliveOk.py
@@ -5,7 +5,7 @@ from Utils.Writer import Writer
 class KeepAliveOk(Writer):
 
     def __init__(self, device):
-        self.id = 24135
+        self.id = 20108
         self.device = device
         super().__init__(self.device)
 
